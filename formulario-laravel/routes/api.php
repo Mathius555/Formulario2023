308ae42723c73ef2b5f7c985ca928312dce5b14a
Mathius555 <matiasrozalez555@gmail.com>
2024-04-21 21:10:24 -0300

    3ty commit

--- formulario-laravel/routes/api.php
+++ formulario-laravel/routes/api.php
@@ -8,5 +8,5 @@
      Route::POST('usuario', [UsuarioController::class, 'store']);//FUNCIONA  http://localhost:8000/api/usuario/
      Route::GET('usuario/{id_usuario}', [UsuarioController::class, 'show']); //FUNCIONA http://localhost:8000/api/usuario/?id_usuario=202
      Route::PUT('usuario/{id_usuario}', [UsuarioController::class, 'update']);//FUNCIONA  http://localhost:8000/api/usuario/202
-     Route::DELETE('usuario/{id_usuario}', [UsuarioController::class, 'destroy']);//FUNCIONA http://localhost:8000/api/usuario/?id_usuario=202
+     Route::DELETE('usuario/{id_usuario}', [UsuarioController::class, 'destroy']);//FUNCIONA http://localhost:8000/api/usuario/202
      
